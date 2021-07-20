07f3892793b58928e58743125e1fadbb8c6cbc7b
CristhianChaucaMariaca <cristhiancchm+git@gmail.com>
2021-07-20 15:49:08 -0400

    Se concluyo el curso y la practica

---
+++ routes/api.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\PacienteController;
+use App\Http\Controllers\API\AutenticarController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+/* Route::get('pacientes',[PacienteController::class, 'index']);
+Route::get('pacientes/{paciente}',[PacienteController::class, 'show']);
+Route::post('pacientes',[PacienteController::class,'store']);
+Route::put('pacientes/{paciente}',[PacienteController::class,'update']);
+Route::delete('pacientes/{paciente}',[PacienteController::class,'destroy']); */
+
+
+
+Route::post('registro',[AutenticarController::class, 'registro']);
+Route::post('acceso',[AutenticarController::class, 'acceso']);
+
+Route::group(['middleware'=>['auth:sanctum']],function(){
+    Route::post('cerrarsesion',[AutenticarController::class, 'cerrarSesion']);
+    Route::apiResource('pacientes',PacienteController::class);
+});
